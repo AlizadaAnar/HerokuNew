ef9d6026ae7268673c356798faef513ae6e7c6c8
anaralizada <anar_alizade@internet.ru>
2024-06-03 14:31:28 +0400

    added

--- src/main/java/com/alibou/security/sharing/SharingEntity.java
+++ src/main/java/com/alibou/security/sharing/SharingEntity.java
@@ -1,24 +1,24 @@
-//package com.alibou.security.sharing;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Entity
-//@Getter
-//@Setter
-//public class SharingEntity {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    private String title;
-//    private String weight;
-//    private String price;
-//    private String region;
-//
-//    private String image;
-//}
+package com.alibou.security.sharing;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class SharingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String weight;
+    private String price;
+    private String region;
+
+    private String image;
+}
